27deea4d9d787bf83c21074ae8e204db6b1d8b05
Diogo <diogo_sergio_ribeiro@hotmail.com>
2016-03-22 20:51:14 -0300

    Aula FX

---
+++ src/br/univel/TelaPrincipalController.java
@@ -0,0 +1,22 @@
+package br.univel;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class TelaPrincipalController {
+	
+	 @FXML
+	    private Button botao;
+
+	    @FXML
+	    private TextField campo;
+
+	    @FXML
+	    void executar(ActionEvent event) {
+
+	    }
+
+	}
+
